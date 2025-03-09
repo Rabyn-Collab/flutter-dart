ea29c64de3825df4894d20daadec692ec6a2983c
Rabyn-Collab <babynshrestha64@gmail.com>
2025-03-09 12:29:36 +0545

    changes

--- controllers/newsController.js
+++ controllers/newsController.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { mainNews } from "../apis.js";
 import { instance } from "../index.js";
 
@@ -8,7 +9,11 @@ import { instance } from "../index.js";
 export const getNews = async (req, res) => {
 
   try {
-    const response = await instance.get(mainNews);
+    const response = await axios.get(mainNews, {
+      headers: {
+        Authorization: 'token d8058afd4d72d56b1cd87497e0c0f8aabc0998b0'
+      }
+    });
     return res.status(200).json(response.data);
   } catch (err) {
     return res.status(400).json({ data: err.data || err.message });
