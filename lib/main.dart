fc52d9c8afe149e9a7be2144d6f45884ac21d12f
Rabyn-Collab <babynshrestha64@gmail.com>
2025-03-14 15:32:46 +0545

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:mvvm/routes/app_routes.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 import 'firebase_options.dart';
 
@@ -17,6 +18,9 @@ void main () async{
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  OneSignal.initialize("8d40620a-e8e0-491a-b772-5bc5cad16deb");
+  OneSignal.Notifications.requestPermission(true);
+
   runApp(ProviderScope(child:const Main()));
 }
 
